993f28eec5868296a8eaa7e9601c7421b86223ac
Abdelatif <76904041+t4tif@users.noreply.github.com>
2021-04-21 19:20:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello i here")
+var next = document.getElementById('bar');
+
+var ArrImage = ["Img/pic02.png","Img/pic03.png","Img/pic04.png","Img/pic05.png","Img/pic06.png","Img/pic07.png"];
+var IndexImage = 0;
+
+function chang() {
+    next.setAttribute("src", ArrImage[IndexImage]);
+    IndexImage++;
+    if (IndexImage>=ArrImage.length) {
+        IndexImage=0;
+    }
+}
+
+//setInterval(chang,4000);
+var stop = setInterval(chang,4000);
+
+sImage.onclick = function () {
+    clearInterval(stop);
+}
